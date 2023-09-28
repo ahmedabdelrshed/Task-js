5c01589ce63c18843833d6e0ed328d7fd50ea9a9
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2023-09-28 23:50:03 +0300

    update

--- JS/script.js
+++ JS/script.js
@@ -26,19 +26,19 @@ var t_price = document.querySelector(".btn-p")
 var price = document.querySelector(".t-p")
 var total = 0
 
-products.forEach(function(product){
-product.onclick = function (){
-    invoice.innerHTML += product.textContent + "<br>"
-    total += +(product.getAttribute("price"))
-    if(invoice != ""){
-    invoice.style.display = "block"
-    document.querySelector(".invoice .title").style.display = "block"
-    document.querySelector(".btn-p").style.display = "block"
+products.forEach(function (product) {
+    product.onclick = function () {
+        invoice.innerHTML += product.textContent + "<br>"
+        total += +(product.getAttribute("price"))
+        if (invoice != "") {
+            invoice.style.display = "block"
+            document.querySelector(".invoice .title").style.display = "block"
+            document.querySelector(".btn-p").style.display = "block"
+        }
     }
-}
 })
 
-t_price.onclick = function(){
+t_price.onclick = function () {
     document.querySelector(".t-price").style.display = "block"
     price.innerHTML = total + "$"
 }
